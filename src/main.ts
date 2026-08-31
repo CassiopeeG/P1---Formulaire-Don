e5b040bb245737c7f870e7447f05e149fb3f8bc8
Cassiopée Gariépy <2442173@csfoy.ca>
2026-08-31 15:48:14 -0400

    Progression de la validation avec Typescript.

--- src/main.ts
+++ src/main.ts
@@ -1,101 +1,155 @@
 import "./style.css";
+interface messageErreur {
+  vide?: string;
+  pattern?: string;
+  type?: string;
+}
+interface erreursJSON {
+  [fieldName: string]: messageErreur;
+}
+let messagesJSON: erreursJSON;
 
-const btnEtape1 = document.getElementById("etape1");
-const btnEtape2 = document.getElementById("etape2");
-const btnEtape3 = document.getElementById("etape3");
-const btnEnvoyer = document.getElementById("envoyer");
+async function obtenirMessages(): Promise<void> {
+  const reponse = await fetch("objJSONMessages.json");
+  messagesJSON = await reponse.json();
+}
 
-const btnNavEtape1 = document.getElementById("navEtapes_etape1");
-const btnNavEtape2 = document.getElementById("navEtapes_etape2");
-const btnNavEtape3 = document.getElementById("navEtapes_etape3");
-const btnNavEtape4 = document.getElementById("navEtapes_etape4");
+const btnEtape1 = document.getElementById("etape1") as HTMLButtonElement | null;
+const btnEtape2 = document.getElementById("etape2") as HTMLButtonElement | null;
+const btnEtape3 = document.getElementById("etape3") as HTMLButtonElement | null;
+const btnEnvoyer = document.getElementById(
+  "envoyer",
+) as HTMLButtonElement | null;
 
-const section1 = document.getElementById("section1");
-const section2 = document.getElementById("section2");
-const section3 = document.getElementById("section3");
-const section4 = document.getElementById("section4");
+const btnNavEtape1 = document.getElementById("navEtapes_etape0");
+const btnNavEtape2 = document.getElementById("navEtapes_etape1");
+const btnNavEtape3 = document.getElementById("navEtapes_etape2");
+const btnNavEtape4 = document.getElementById("navEtapes_etape3");
+
+let etape: number;
+const sections = new Array();
+sections.push(document.getElementById("section0"));
+sections.push(document.getElementById("section1"));
+sections.push(document.getElementById("section2"));
+sections.push(document.getElementById("section3"));
+
+//ÉLÉMENTS DU FORMULAIRE
+
+//Type Radio
+//0
+const refVersement = document.getElementsByName("versement");
+const refMontant = document.getElementsByName("montant");
+
+//Type Number
+//0
+const refMontantPerso = document.getElementById("montantPerso");
+//2
+const refNumeroCarte = document.getElementById("numeroCarte");
+const refCodeSecuriteCarte = document.getElementById("codeSecuriteCarte");
+
+// Type Select
+//2
+const refMoisExpiration = document.getElementById("moisExpiration");
+const refAnneeExpiration = document.getElementById("anneeExpiration");
+
+//Type Text
+//1
+const refNom = document.getElementById("nom");
+const refPrenom = document.getElementById("prenom");
+const refEmail = document.getElementById("email");
+const refTelephone = document.getElementById("telephone");
+const refAdresse = document.getElementById("adresse");
+const refVille = document.getElementById("ville");
+const refProvince = document.getElementById("province");
+const refCodePostal = document.getElementById("codePostal");
 
 function initialiser(): void {
-    if (section1) {
-    section1.classList.remove("hidden");
-  }
-  if (section2) {
-    section2.classList.add("hidden");
-  }
-  if (section3) {
-    section3.classList.add("hidden");
-  }
-  if (section4) {
-    section4.classList.add("hidden");
+  etape = 0;
+  afficherEtape();
+}
+
+function afficherEtape(): void {
+  for (let index = 0; index < sections.length; index++) {
+    //Si la section correspond à l'étape visé, on affiche cette section
+    if (sections[index]) {
+      if (index == etape) {
+        sections[index].classList.remove("sr-only");
+        //Sinon, cacher les autres sections du formulaire
+      } else {
+        sections[index].classList.add("sr-only");
+      }
+    }
   }
 }
 
-function passerEtape1(event: MouseEvent): void {
-  console.log("fonction passerEtape1");
+//ICI MODIFIER CODE DONNÉ, POUR VALIDER LES MONTANTS - RN VALIDE INFOS, pas ARGENT
+//TESTER ET VALIDER
+function validerEtape0() {
+  console.log("Passé suivant, etape = " + etape);
+  afficherEtape();
 
-  if (section1) {
-    section1.classList.remove("hidden");
-  }
-  if (section2) {
-    section2.classList.add("hidden");
-  }
-  if (section3) {
-    section3.classList.add("hidden");
-  }
-  if (section4) {
-    section4.classList.add("hidden");
+  const nomElement = document.getElementById("nom") as HTMLInputElement;
+  const prenomElement = document.getElementById("prenom") as HTMLInputElement;
+  const emailElement = document.getElementById("email") as HTMLInputElement;
+  const telephoneElement = document.getElementById(
+    "telephone",
+  ) as HTMLInputElement;
+
+  const nomValide = validerChamp(nomElement);
+  const prenomValide = validerChamp(prenomElement);
+  const emailValide = validerChamp(emailElement);
+  const telephoneValide = validerChamp(telephoneElement);
+
+  if (!nomValide || !prenomValide || !emailValide || !telephoneValide) {
+    console.log("Attention, erreur");
+  } else {
+    etape++;
   }
 }
 
-function passerEtape2(event: MouseEvent): void {
-  console.log("fonction passerEtape2");
+function validerEtape1() {}
 
-  if (section1) {
-    section1.classList.add("hidden");
+function validerEtape2() {}
+
+function validerChamp(champ: HTMLInputElement): boolean {
+  let valide = false;
+  const id = champ.id; // email
+  const idMessageErreur = "erreur-" + id; // erreur-email
+  const erreurElement = document.getElementById(
+    idMessageErreur,
+  ) as HTMLDivElement;
+
+  console.log("valider champ", champ.validity);
+
+  // Vérifie chaque type d'erreur de validation
+  if (champ.validity.valueMissing && messagesJSON[id].vide) {
+    console.log("erreur", id);
+
+    valide = false;
+    erreurElement.innerText = messagesJSON[id].vide;
+  } else if (champ.validity.typeMismatch && messagesJSON[id].type) {
+    // Type de données incorrect (email, url, tel, etc.)
+    valide = false;
+    erreurElement.innerText = messagesJSON[id].type;
+  } else if (champ.validity.patternMismatch && messagesJSON[id].pattern) {
+    // Ne correspond pas au pattern regex défini
+    valide = false;
+    erreurElement.innerText = messagesJSON[id].pattern;
+  } else {
+    // La validation n'a pas d'erreur, donc on assigne la variable vraie
+    valide = true;
   }
-  if (section2) {
-    section2.classList.remove("hidden");
-  }
-  if (section3) {
-    section3.classList.add("hidden");
-  }
-  if (section4) {
-    section4.classList.add("hidden");
-  }
+
+  return valide;
 }
 
-function passerEtape3(event: MouseEvent): void {
-  console.log("fonction passerEtape3");
+function revenirEtapePrecedente(event: MouseEvent): void {
+  const target = event.currentTarget as HTMLElement;
+  const nbrEtapeVisee: number = parseInt(target.id.slice(15));
 
-  if (section1) {
-    section1.classList.add("hidden");
-  }
-  if (section2) {
-    section2.classList.add("hidden");
-  }
-  if (section3) {
-    section3.classList.remove("hidden");
-  }
-  if (section4) {
-    section4.classList.add("hidden");
-  }
-}
-
-function passerEtape4(event: MouseEvent): void {
-  console.log("fonction passerEtape4");
-
-  if (section1) {
-    section1.classList.add("hidden");
-  }
-  if (section2) {
-    section2.classList.add("hidden");
-  }
-  if (section3) {
-    section3.classList.add("hidden");
-  }
-  if (section4) {
-    section4.classList.remove("hidden");
-    console.log(section4.classList);
+  if (nbrEtapeVisee <= etape) {
+    etape = nbrEtapeVisee;
+    afficherEtape();
   }
 }
 
@@ -105,34 +159,30 @@ function envoyer(event: MouseEvent): void {
 
 //addEventListener des boutons "suivant"
 if (btnEtape1) {
-  btnEtape1.addEventListener("click", passerEtape2);
+  btnEtape1.addEventListener("click", validerEtape0);
 }
 if (btnEtape2) {
-  btnEtape2.addEventListener("click", passerEtape3);
+  btnEtape2.addEventListener("click", validerEtape1);
 }
 if (btnEtape3) {
-  btnEtape3.addEventListener("click", passerEtape4);
+  btnEtape3.addEventListener("click", validerEtape2);
+  if (btnEnvoyer) {
+    btnEnvoyer.addEventListener("click", envoyer);
+  }
 }
 
 //addEVentListener des boutons de navigation d'étapes
-if(btnNavEtape1){
-  btnNavEtape1.addEventListener("click", passerEtape1);
+if (btnNavEtape1) {
+  btnNavEtape1.addEventListener("click", revenirEtapePrecedente);
 }
-
-if(btnNavEtape2){
-  btnNavEtape2.addEventListener("click", passerEtape2);
+if (btnNavEtape2) {
+  btnNavEtape2.addEventListener("click", revenirEtapePrecedente);
 }
-
-if(btnNavEtape3){
-  btnNavEtape3.addEventListener("click", passerEtape3);
+if (btnNavEtape3) {
+  btnNavEtape3.addEventListener("click", revenirEtapePrecedente);
 }
-
-if(btnNavEtape4){
-  btnNavEtape4.addEventListener("click", passerEtape4);
-}
-
-if (btnEnvoyer) {
-  btnEnvoyer.addEventListener("click", envoyer);
+if (btnNavEtape4) {
+  btnNavEtape4.addEventListener("click", revenirEtapePrecedente);
 }
 
 initialiser();
